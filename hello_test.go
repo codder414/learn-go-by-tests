eaf7f98f8dfe0025bcf3699090e168eee43614ec
Nurik <Nurik_Nurmetov@epam.com>
2021-10-30 17:42:09 +0500

    feat: refactor tests

--- hello_test.go
+++ hello_test.go
@@ -3,10 +3,26 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris!"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	assertMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+
 	}
+
+	t.Run("saying hello to people", func(*testing.T) {
+		got := Hello("")
+		want := englishHelloPrefix + "World" + "!"
+
+		assertMessage(t, got, want)
+	})
+
+	t.Run("saying hello to Chris", func(*testing.T) {
+		got := Hello("Chris")
+		want := englishHelloPrefix + "Chris" + "!"
+		assertMessage(t, got, want)
+	})
+
 }
